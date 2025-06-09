8f0cd6403e2982c47bd7f04a090219a47a563d1c
nate <ncgaaran@gmail.com>
2025-06-09 20:55:31 -0300

    comprimido tudo

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
-const gulp=require('gulp');
-const sass=require('gulp-sass')(require('sass'));
-const imagemin=require('gulp-imagemin');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css'); // Adicione este pacote
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({outputStyle:'compressed'}))
+        .pipe(sass({
+            outputStyle: 'compressed',
+        }).on('error', sass.logError))
+        .pipe(cleanCSS({
+            level: 2 // Otimização mais agressiva
+        }))
         .pipe(gulp.dest('./dist/css'))
 }
 
@@ -14,7 +27,8 @@ function images() {
         .pipe(gulp.dest('./dist/images'))
 }
 
-exports.default=gulp.parallel(styles, images);
-exports.watch=function(){
+exports.default = gulp.parallel(styles, images, scripts);
+exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
